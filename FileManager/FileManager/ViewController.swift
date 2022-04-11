8f54525479a58ef25537928f2c87963a751d5ce7
Berat Altuntaş <cberataltuntas>
2022-04-12 01:02:51 +0300

    file manager ve documentPicker ornegi bitti.

--- FileManager/FileManager/ViewController.swift
+++ FileManager/FileManager/ViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import MobileCoreServices //
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDocumentPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +18,7 @@ içeriği burada yazanlardır.
 
 """
         let dosyaAdi = "dosyaAdi.txt"
-        createFile(withContent: icerik,fileName: dosyaAdi)
+        //createFile(withContent: icerik,fileName: dosyaAdi)
         readFile(withFileName: dosyaAdi)
     }
 
@@ -31,10 +32,11 @@ içeriği burada yazanlardır.
                 try content.write(to: fileUrl,atomically: false,encoding:.utf8)
             }
             catch{
-                print("hata")
+                print("error")
             }
         }
     }
+    
     
     func readFile(withFileName fileName: String){
         if let directoryUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
@@ -45,9 +47,32 @@ içeriği burada yazanlardır.
                 print(read)
             }
             catch{
-                
+                print("error")
             }
         }
+    }
+    
+    @IBAction func openFile_TUI(_ sender: Any) {
+        let dpVC = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText)], in: .import)
+        dpVC.delegate = self
+        dpVC.modalPresentationStyle = .formSheet
+        present(dpVC, animated: true)
+        
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        do{
+            let data = try Data(contentsOf: urls[0])
+            let content = String(data: data, encoding: .utf8)
+            print(content!)
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("vazgeçildi.")
     }
 
 
