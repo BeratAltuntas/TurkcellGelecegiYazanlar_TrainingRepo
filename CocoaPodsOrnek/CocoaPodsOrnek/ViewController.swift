a8a40e74f0d390838f42b9eae84c573440c5bbea
Berat Altuntaş <cberataltuntas>
2022-04-12 23:27:05 +0300

    json kutuphanesi ile json veri cekme ornegi yapildi.

--- CocoaPodsOrnek/CocoaPodsOrnek/ViewController.swift
+++ CocoaPodsOrnek/CocoaPodsOrnek/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var persons = [Person]()
+        let jsonStr = "[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},        {\"firstName\":\"Peter\", \"lastName\":\"Jones\"}]"
+        
+        if let jsonData = jsonStr.data(using: .utf8){
+            let Json = JSON(jsonData)
+            var person : Person
+            for j in Json.arrayValue{
+                person = Person(Name: j["firstName"].stringValue, Lastname: j["lastName"].stringValue)
+                persons.append(person)
+            }
+            
+        
+        }
+        
+        for person in persons {
+            print(person.firstName , person.lastname)
+        }
     }
 
 
+}
+
+class Person{
+    var firstName : String
+    var lastname: String
+    
+    init(Name: String, Lastname: String){
+        firstName = Name
+        lastname = Lastname
+    }
 }
 
