c226cef9409ebfead0d1d09ab588904e9b620f7d
Berat Altuntaş <cberataltuntas@gmail.com>
2022-04-22 17:56:17 +0300

    PushNotification ornek yapıldı.

---
+++ PushNotification/PushNotification/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  PushNotification
+//
+//  Created by BERAT ALTUNTAŞ on 22.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { authorizationStatus, error in
+            if authorizationStatus{
+                print("Authorization Given")
+            }else{
+                print("Authorization Rejected")
+            }
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ PushNotification/PushNotification/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PushNotification
+//
+//  Created by BERAT ALTUNTAŞ on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func pushNotification_TUI(_ sender: Any) {
+        let notificationContent = UNMutableNotificationContent() // Notification oluşturmak için içeriğin nelerden oluştuğunu belirtiyor
+        notificationContent.title = "bildirim title"// başlık
+        notificationContent.subtitle = "bildirim subtitle"//altbaşlık
+        notificationContent.body = "bildirim body"//metin
+        
+        let whenIsTriggering = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) // bildirimin ne kadar zamanda bir veya ne zaman gönderileceğini ayarlıyor.
+        
+        let notification = UNNotificationRequest(identifier: "notificationButton", content: notificationContent, trigger: whenIsTriggering)// yukarıda belirttiğimiz içerikte ve belirtilen sürede bir notification isteği oluşturuyor
+        
+        UNUserNotificationCenter.current().add(notification)// bu method yukarıda oluşturduğumuz isteği bildirim gönderebilmesi için kayıt ediyor, ekliyor UNUserNotificationCenter'a
+    }
+    
+}
+
